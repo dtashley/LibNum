2948f63459089c01bcf2016bd7fef4aeab866e51
David T. Ashley <dashley@gmail.com>
2024-03-30 16:41:39 -0400

    Rename library from UcuLib to LibNum.

--- doc/userman/recomp.bat
+++ doc/userman/recomp.bat
@@ -2,11 +2,11 @@ rem ---------------------------------------------------------------------------
 rem Fully builds the document.  The commands below are run three times to
 rem ensure that all references are resolved.
 rem ---------------------------------------------------------------------------
-latex ucu_userman
-makeindex ucu_userman
-latex ucu_userman
-makeindex ucu_userman
-latex ucu_userman
-makeindex ucu_userman
-dvips -T 8.5in,11in ucu_userman
+latex libnum_userman
+makeindex libnum_userman
+latex libnum_userman
+makeindex libnum_userman
+latex libnum_userman
+makeindex libnum_userman
+dvips -T 8.5in,11in libnum_userman
 rem ---------------------------------------------------------------------------
--- doc/userman/recomp_minimal.bat
+++ doc/userman/recomp_minimal.bat
@@ -3,5 +3,5 @@ rem Minimal build for iterative editing and using the .DVI file only.
 rem Index and bibliography not rebuilt, PostScript output mot refreshed.
 rem Must be run twice to refresh any changed LaTeX references.
 rem ---------------------------------------------------------------------------
-latex ucu_userman
+latex libnum_userman
 rem ---------------------------------------------------------------------------
