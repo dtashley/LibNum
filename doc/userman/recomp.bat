72aa6cdb93274309a5a9fd662c70b2906d02d396
David T. Ashley <dashley@gmail.com>
2024-03-30 21:18:39 -0400

    Rename.

--- doc/userman/recomp.bat
+++ doc/userman/recomp.bat
@@ -2,11 +2,11 @@ rem ---------------------------------------------------------------------------
 rem Fully builds the document.  The commands below are run three times to
 rem ensure that all references are resolved.
 rem ---------------------------------------------------------------------------
-latex libnum_userman
-makeindex libnum_userman
-latex libnum_userman
-makeindex libnum_userman
-latex libnum_userman
-makeindex libnum_userman
-dvips -T 8.5in,11in libnum_userman
+latex libnum_user_manual
+makeindex libnum_user_manual
+latex libnum_user_manual
+makeindex libnum_user_manual
+latex libnum_user_manual
+makeindex libnum_user_manual
+dvips -T 8.5in,11in libnum_user_manual
 rem ---------------------------------------------------------------------------
--- doc/userman/recomp_minimal.bat
+++ doc/userman/recomp_minimal.bat
@@ -3,5 +3,5 @@ rem Minimal build for iterative editing and using the .DVI file only.
 rem Index and bibliography not rebuilt, PostScript output mot refreshed.
 rem Must be run twice to refresh any changed LaTeX references.
 rem ---------------------------------------------------------------------------
-latex libnum_userman
+latex libnum_user_manual
 rem ---------------------------------------------------------------------------
